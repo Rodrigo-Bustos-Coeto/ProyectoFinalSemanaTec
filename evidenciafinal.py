2075ee84636ad188dce9770bd6b44f9d213be22d
Rodrigo-Bustos-Coeto <a01645656@tec.mx>
2025-10-29 14:45:05 -0600

    Update evidenciafinal.py

--- evidenciafinal.py
+++ evidenciafinal.py
@@ -17,7 +17,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv2.imread("jitomate1.jpg")
+img = cv2.imread("./imagenes/jitomate1.jpg")
 
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
